3f86aece6fe6ad24abab6860965e937589288f81
Emre Savaş <emre_savas28@hotmail.com>
2023-12-16 00:18:40 +0300

    Update RepositoryContext to inherit from IdentityDbContext.

--- Repositories/EFCore/RepositoryContext.cs
+++ Repositories/EFCore/RepositoryContext.cs
@@ -1,15 +1,15 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Repositories.EFCore
 {
-    public class RepositoryContext : DbContext
+    public class RepositoryContext : IdentityDbContext<User>
     {
         public RepositoryContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Adress> Adresses { get; set; }
         public DbSet<Geo> Geos { get; set; }
         public DbSet<Company> Companies { get; set; }
