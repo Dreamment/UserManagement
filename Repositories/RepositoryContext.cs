1aeea7b4d46dfed2e5950668245fe9e8fcb10dca
Emre Savaş <emre_savas28@hotmail.com>
2023-12-15 21:04:03 +0300

    Add RepositoryContext.cs .

---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    internal class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Geo> Geos { get; set; }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
