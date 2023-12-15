6978057c6cfd38e8146b092b822ee5e63fe2ec3e
Emre Savaş <emre_savas28@hotmail.com>
2023-12-15 23:59:10 +0300

    Add RepositoryManager.

---
+++ Repositories/RepositoryManager.cs
@@ -0,0 +1,21 @@
+﻿using Repositories.Contracts;
+using Repositories.EFCore;
+
+namespace Repositories
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+
+            User = new UserRepository(_context);
+        }
+
+        public IUserRepository User { get; }
+
+        public Task SaveAsync()
+            => _context.SaveChangesAsync();
+    }
+}
