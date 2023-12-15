6e004cdc0ba0e3c4a9138ea953ab271fecfb2a25
Emre Savaş <emre_savas28@hotmail.com>
2023-12-15 23:42:38 +0300

    Add UserRepository.

---
+++ Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using System.Linq.Expressions;
+
+namespace Repositories.Contracts
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+        Task<User> GetUserByIdAsync(int userId, bool trackChanges);
+        Task<User> GetUserByIdWithDetailsAsync(int userId, bool trackChanges, params Expression<Func<User, object>>[] includes);
+        Task<User> GetUserByEMailAsync(string email, bool trackChanges);
+        Task<User> GetUserByEmailWithDetailsAsync(string email, bool trackChanges, params Expression<Func<User, object>>[] includes);
+        Task<User> GetUserByUserNameAsync(string userName, bool trackChanges);
+        Task<User> GetUserByUserNameWithDetailsAsync(string userName, bool trackChanges, params Expression<Func<User, object>>[] includes);
+        Task CreateUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(User user);
+
+    }
+}
---
+++ Repositories/EFCore/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(RepositoryContext context) : base(context)
+        {
+
+        }
+
+        public async Task CreateUser(User user)
+            => await CreateAsync(user);
+
+        public async Task DeleteUser(User user)
+            => await DeleteAsync(user);
+
+        public async Task<User> GetUserByEMailAsync(string email, bool trackChanges)
+        {
+            var user = await FindByConditionAsync(u => u.Email.Equals(email), trackChanges);
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetUserByEmailWithDetailsAsync(string email, bool trackChanges, params Expression<Func<User, object>>[] includes)
+        {
+            var user = await FindByConditionWithDetailsAsync(u => u.Email.Equals(email), trackChanges, includes);
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetUserByIdAsync(int userId, bool trackChanges)
+        {
+            var user = await FindByConditionAsync(u => u.Id.Equals(userId), trackChanges);
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetUserByIdWithDetailsAsync(int userId, bool trackChanges, params Expression<Func<User, object>>[] includes)
+        {
+            var user = await FindByConditionWithDetailsAsync(u => u.Id.Equals(userId), trackChanges, includes);
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetUserByUserNameAsync(string userName, bool trackChanges)
+        {
+            var user = await FindByConditionAsync(u => u.UserName.Equals(userName), trackChanges);
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetUserByUserNameWithDetailsAsync(string userName, bool trackChanges, params Expression<Func<User, object>>[] includes)
+        {
+            var user = await FindByConditionWithDetailsAsync(u => u.UserName.Equals(userName), trackChanges, includes);
+            return user.FirstOrDefault();
+        }
+
+        public async Task UpdateUser(User user)
+            => await UpdateAsync(user);
+    }
+}
