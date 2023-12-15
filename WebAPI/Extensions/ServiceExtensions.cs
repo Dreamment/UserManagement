5f30dc6f10586a946a20625cb78932e47b89e4e8
Emre Savaş <emre_savas28@hotmail.com>
2023-12-16 00:53:40 +0300

    Add dependency injection for IRepositoryManager.

---
+++ WebAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Repositories;
+using Repositories.Contracts;
+
+namespace WebAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+    }
+}
