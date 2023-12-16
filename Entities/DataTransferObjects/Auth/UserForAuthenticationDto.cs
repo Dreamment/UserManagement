a0a8ba407b76b916564e7ca8730507c9efd4fcbd
Emre Savaş <emre_savas28@hotmail.com>
2023-12-16 17:29:10 +0300

    Add Auth Dtos.

---
+++ Entities/DataTransferObjects/Auth/UserForAuthenticationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Entities.DataTransferObjects.Auth
+{
+    public class UserForAuthenticationDto
+    {
+        [AllowNull]
+        public string? Email { get; set; }
+        [AllowNull]
+        public string? UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
