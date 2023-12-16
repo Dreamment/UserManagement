2f4aa09c1fb03aed0713f4bdde9c7e9b58fb4da1
Emre Savaş <emre_savas28@hotmail.com>
2023-12-16 10:44:22 +0300

    Add dependency injection for IUserService.

--- WebAPI/Extensions/ServiceExtensions.cs
+++ WebAPI/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Repositories;
 using Repositories.Contracts;
+using Services;
+using Services.Contracts;
 
 namespace WebAPI.Extensions
 {
@@ -8,6 +10,7 @@ namespace WebAPI.Extensions
         public static void ConfigureCustomServices(this IServiceCollection services)
         {
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IUserService, UserManager>();
         }
     }
 }
