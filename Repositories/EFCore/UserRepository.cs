7e81c000a6ac3699f0f7485cc34e00dd2530147d
Emre Savaş <emre_savas28@hotmail.com>
2023-12-17 12:57:18 +0300

    Remove unnecassary usings.

--- Repositories/EFCore/UserRepository.cs
+++ Repositories/EFCore/UserRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Entities.Models;
 using Repositories.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
