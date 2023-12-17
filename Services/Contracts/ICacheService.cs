25a764a8575b58aa83dfebd113141ecbaa493c14
Emre Savaş <emre_savas28@hotmail.com>
2023-12-17 12:54:37 +0300

    Implement RedisCacheManager.

---
+++ Services/Contracts/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Contracts
+{
+    public interface ICacheService
+    {
+        Task<bool> SetValueAsync(string key, string value);
+        Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> action) where T : class;
+        Task ClearAsync(string key);
+    }
+}
---
+++ Services/RedisCacheManager.cs
@@ -0,0 +1,38 @@
+﻿using Services.Contracts;
+using StackExchange.Redis;
+using System.Text;
+using System.Text.Json;
+
+namespace Services
+{
+    public class RedisCacheManager : ICacheService
+    {
+        private readonly IConnectionMultiplexer _redisCon;
+        private readonly IDatabase _cache;
+        private TimeSpan ExpireTime = TimeSpan.FromMinutes(60);
+
+        public RedisCacheManager(IConnectionMultiplexer redisCon)
+        {
+            _redisCon = redisCon;
+            _cache = redisCon.GetDatabase();
+        }
+
+        public async Task ClearAsync(string key)
+            => await _cache.KeyDeleteAsync(key);
+
+        public async Task<T> GetOrAddAsync<T>(string key, Func<Task<T>> action) where T : class
+        {
+            var result = await _cache.StringGetAsync(key);
+            if (result.IsNull)
+            {
+                result = JsonSerializer.SerializeToUtf8Bytes(await action());
+                await SetValueAsync(key, result);
+            }
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+
+        public async Task<bool> SetValueAsync(string key, string value)
+            => await _cache.StringSetAsync(key, value, ExpireTime);
+    }
+}
