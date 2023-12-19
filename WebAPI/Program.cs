a31e1cb22babeac1dc91ce675bccba581daeea94
Emre Savaş <emre_savas28@hotmail.com>
2023-12-19 16:23:37 +0300

    Add Docker container support with network includes MSSQL, Redis and WebAPI; remove HTTPS redirection.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -21,21 +21,19 @@ builder.Services.ConfigureRedis(builder.Configuration);
 
 var app = builder.Build();
 
-// if you want to use swagger in darkmode uncomment the following line it will open in almost 30 seconds delayed
+// if you want to use swagger in darkmode uncomment the following line it will open in almost 5-10 seconds delayed
 // after open or close dark mode rebuild the solution
-app.UseStaticFiles();
+//app.UseStaticFiles();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(s =>
     {
         s.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagementWebAPI v1");
-        s.InjectStylesheet("/swagger-ui/SwaggerDark.css");
+        //s.InjectStylesheet("/swagger-ui/SwaggerDark.css");
     });
 }
-// you can use this url to open swagger if it will open late : https://localhost:7288/swagger/index.html
-
-app.UseHttpsRedirection();
+// you can use this url to open swagger if it will open late : https://localhost:8080/swagger/index.html
 
 app.UseAuthentication();
 app.UseAuthorization();
