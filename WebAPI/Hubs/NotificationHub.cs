b5218825ee4a77b74aa9ce732143d539efe6fc1d
Emre Savaş <emre_savas28@hotmail.com>
2024-01-07 12:25:02 +0300

    Add NotificationHub.

---
+++ WebAPI/Hubs/NotificationHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace WebAPI.Hubs
+{
+    [Authorize]
+    public class NotificationHub : Hub
+    {
+        public async override Task OnConnectedAsync()
+        {
+            Thread.Sleep(1000);
+            try
+            {
+                var claim = Context.User?.FindFirst(ClaimTypes.Role)?.Value;
+                var name = Context.User?.FindFirst(ClaimTypes.Name)?.Value;
+                await Groups.AddToGroupAsync(Context.ConnectionId, $"{claim}");
+                await Clients.Caller.SendAsync("Connected", $"You are connected as {name} and your role is {claim}");
+            }
+            catch (Exception ex)
+            {
+                await Clients.Caller.SendAsync("Error", ex.Message);
+            }
+        }
+
+    }
+}
