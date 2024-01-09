2280087f24a7da4739d3aea7d4a0370e01528cfd
Emre Savaş <emre_savas28@hotmail.com>
2024-01-09 17:38:02 +0300

    Build app and configure MassTransit.

--- MailSender/Program.cs
+++ MailSender/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello World!");
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureAppConfiguration((hostContext, configApp) =>
+{
+    configApp.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+});
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    services.AddMassTransit(busConfigurator =>
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        busConfigurator.AddConsumers(entryAssembly);
+        busConfigurator.UsingRabbitMq((context, busFactoryConfigurator) =>
+        {
+            var connectionString = hostContext.Configuration.GetConnectionString("rabbitMQConnection");
+            busFactoryConfigurator.Host(connectionString, "/", hostConfigurator => { });
+
+            busFactoryConfigurator.ConfigureEndpoints(context);
+        });
+    });
+});
+
+var app = builder.Build();
+
+app.Run();
