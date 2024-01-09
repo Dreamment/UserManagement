4a441415b6a379590bb95fb8911602afb780d1a0
Emre Savaş <emre_savas28@hotmail.com>
2024-01-09 17:22:44 +0300

    Add SendMailMessage class.

---
+++ Entities/Messages/SendMailMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Messages
+{
+    public class SendMailMessage
+    {
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+    }
+}
