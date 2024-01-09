99ecec1adf18db534a37424d2e496b94528c3b38
Emre Savaş <emre_savas28@hotmail.com>
2024-01-09 17:41:41 +0300

    Add MailSendException.

---
+++ Entities/Exceptions/MailSend/MailSendException.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions.MailSend
+{
+    public class MailSendException : Exception
+    {
+        public MailSendException(string message) : base(message)
+        {
+            
+        }
+    }
+}
